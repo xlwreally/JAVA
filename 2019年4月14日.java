525bf05768df8f94f45fcb28b56df8d87cbc2600
xlw <42504909+xlwreally@users.noreply.github.com>
2019-04-14 14:13:17 +0800

    Create 2019年4月14日.java

---
+++ 2019年4月14日.java
@@ -0,0 +1,15 @@
+package 你好;
+
+public class dfas {
+	public static void main(String[] args) {
+		int a=1,b=2;
+		while(a==1)
+		{
+		 
+		 b+=100000;
+		 System.out.println("你们好鸭");	
+		}
+       
+    }
+}
+
